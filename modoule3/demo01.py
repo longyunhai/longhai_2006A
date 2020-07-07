6f865ba0ae41576657edcbdf053672fd84a621e3
lonyunhai <1982964634@qq.com>
2020-07-07 16:31:53 +0800

    nice to meet you

--- modoule3/demo01.py
+++ modoule3/demo01.py
@@ -1,4 +1,11 @@
-for x in range(1,10):
-    for y in range(1,x+1):
-        print("{}X{}={}".format(y,x,x*y),end="\t")
-    print()
+# for x in range(1,10):
+#     for y in range(1,x+1):
+# #         print("{}X{}={}".format(y,x,x*y),end="\t")
+# #     print()
+# # print(sum(range(1,100)))
+# # a = 10
+# # def aaa():
+# #     global a
+# #     a = 20
+# # aaa()
+# # print(a)
